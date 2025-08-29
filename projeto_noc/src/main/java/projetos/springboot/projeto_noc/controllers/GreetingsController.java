01033caa9d90a57ee62db49d7b54adc7268df699
Edson <edson.henrique.dev@gmail.com>
2025-08-29 20:34:50 -0300

    Adicionando endpoints para listar usuários e para salvar no banco de dados.

--- projeto_noc/src/main/java/projetos/springboot/projeto_noc/controllers/GreetingsController.java
+++ projeto_noc/src/main/java/projetos/springboot/projeto_noc/controllers/GreetingsController.java
@@ -1,10 +1,17 @@
 package projetos.springboot.projeto_noc.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -60,5 +67,29 @@ public class GreetingsController {
     	return "Olá" + texto;
     	
     }
+    
+   //endpoint para listar os dados (GET - faz buscas)
+   @GetMapping(value = "listar") /*faz a mesma coisa do @RequestMapping, porém já indica que o método irá responder a requisições HTTP GET*/
+   @ResponseBody //retorna o corpo da resposta (o corpo é como se fosse uma caixinha com os dados dentro)
+   public ResponseEntity<List<UsuarioModel>> listarUsuario() {//método retorna uma lista
+    	
+    	List<UsuarioModel> usuarios = usuarioRepository.findAll();//executa e consulta no banco de dados
+    	
+    	return new ResponseEntity<List<UsuarioModel>>(usuarios, HttpStatus.OK);//retorna a lista em json
+    	//usuarios são o corpo da resposta (os dados); HttpStatus é o status HTTP, que 200 (ok)
+    	
+    }
+   
+   @PostMapping(value = "salvar")
+   @ResponseBody//retorna o corpo da resposta (o corpo é como se fosse uma caixinha com os dados dentro)
+   public ResponseEntity<UsuarioModel> salvarUsuario(@RequestBody UsuarioModel usuarioModel) {//@ResponseBody recebe o corpo da resposta (dados) para salvar
+	   
+	   UsuarioModel usuario = usuarioRepository.save(usuarioModel);//salva meus dados no banco
+	   
+	   return new ResponseEntity<UsuarioModel>(usuario, HttpStatus.CREATED);//retorna meus dados salvos
+	 //usuarios são o corpo da resposta; HttpStatus é o status HTTP, que 201 (created)
+	   
+   }
+   
     
 }
